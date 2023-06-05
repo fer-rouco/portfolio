e046518ced0d8b3507423c1e7e59f2d8bf2b3dab
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2023-06-05 16:03:21 -0300

    Changing HTML tag href

--- client/src/components/general/tag.jsx
+++ client/src/components/general/tag.jsx
@@ -1,7 +1,7 @@
 import "./tag.scss";
 
 export const TECH = Object.freeze({
-  HTML: { backgroundColor: '#FF6D00', color: '#F0F0F0', text: 'HTML', tooltip: 'HTML5', href: 'https://html.spec.whatwg.org/multipage/' },
+  HTML: { backgroundColor: '#FF6D00', color: '#F0F0F0', text: 'HTML', tooltip: 'HTML5', href: 'https://html.spec.whatwg.org/dev/' },
   CSS3: { backgroundColor: '#049BE5', color: '#F0F0F0', text: 'CSS', tooltip: 'CSS3', href: 'https://www.css3.com/' },
   JavaScript: { backgroundColor: '#FED600', color: '#303841', text: 'JS', tooltip: 'JavaScript', href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
   React: { backgroundColor: '#88B4ED', color: '#000000', text: 'React', tooltip: 'ReactJs', href: 'https://reactjs.org/' },
