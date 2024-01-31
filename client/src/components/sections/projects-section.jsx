78b1d615a3d785d48337118d1dc0713052d22f95
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2024-01-31 12:26:04 -0300

    github url update

--- client/src/components/sections/projects-section.jsx
+++ client/src/components/sections/projects-section.jsx
@@ -7,30 +7,30 @@ const PROJECTS = [
     key: 'markdown-previewer',
     title:'Markdown Previewer',
     description:'App to edit and previsualize your markdowns.',
-    srcImg:'https://raw.githubusercontent.com/fernet87/markdown-previewer/main/src/assets/screenshot.png',
+    srcImg:'https://raw.githubusercontent.com/fer-rouco/markdown-previewer/main/src/assets/screenshot.png',
     altImg:'Markdow Previewer Image',
-    hrefCode:'https://github.com/fernet87/markdown-previewer/',
-    hrefLive:'https://fernet87.github.io/markdown-previewer/',
+    hrefCode:'https://github.com/fer-rouco/markdown-previewer/',
+    hrefLive:'https://fer-rouco.github.io/markdown-previewer/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.Sass]
   },
   {
     key: 'drum-machine',
     title:'Drum Machine',
     description:'App to play the drum with your keyboard.',
-    srcImg:'https://raw.githubusercontent.com/fernet87/drum-machine/main/src/assets/screenshot.png',
+    srcImg:'https://raw.githubusercontent.com/fer-rouco/drum-machine/main/src/assets/screenshot.png',
     altImg:'Drum Machine Image',
-    hrefCode:'https://github.com/fernet87/drum-machine/',
-    hrefLive:'https://fernet87.github.io/drum-machine/',
+    hrefCode:'https://github.com/fer-rouco/drum-machine/',
+    hrefLive:'https://fer-rouco.github.io/drum-machine/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.StyledComponents]
   },
   {
     key: 'javascript-calculator',
     title:'Javascript Calculator',
     description:'App that allows you to do your arithmetic calculations.',
-    srcImg:'https://raw.githubusercontent.com/fernet87/calculator/main/src/assets/screenshot.png',
+    srcImg:'https://raw.githubusercontent.com/fer-rouco/calculator/main/src/assets/screenshot.png',
     altImg:'Javascript Calculator Image',
-    hrefCode:'https://github.com/fernet87/calculator/',
-    hrefLive:'https://fernet87.github.io/calculator/',
+    hrefCode:'https://github.com/fer-rouco/calculator/',
+    hrefLive:'https://fer-rouco.github.io/calculator/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Sass]
   }
 ];
