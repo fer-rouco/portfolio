21efd7eabcb0878b94b5420b1d97fd3fd184f36d
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2024-02-01 17:27:43 -0300

    Carousel implemented in project cards

--- client/src/components/sections/projects-section.jsx
+++ client/src/components/sections/projects-section.jsx
@@ -1,6 +1,8 @@
 import Card from "../general/card";
 import { TECH } from "../general/tag";
 import "./projects-section.scss";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const PROJECTS = [
   {
@@ -42,28 +44,85 @@ const PROJECTS = [
     hrefCode:'https://github.com/fer-rouco/calculator/',
     hrefLive:'https://fer-rouco.github.io/calculator/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Sass]
+  },
+  {
+    key: 'pomodoro-clock',
+    title:'Pomodoro 25 + 5 Clock',
+    description:'App to apply the Pomodoro Technique to your daily tasks.',
+    srcImg:'https://raw.githubusercontent.com/fer-rouco/25-5-clock/main/src/assets/screenshot.png',
+    altImg:'Pomodoro (25+5) Clock Image',
+    hrefCode:'https://github.com/fer-rouco/25-5-clock/',
+    hrefLive:'https://fer-rouco.github.io/25-5-clock/',
+    tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.Sass]
+  },
+  {
+    key: 'random-quote-machine',
+    title:'Random Quote Machine',
+    description:'App which randomly shows quotes from different authors.',
+    srcImg:'https://raw.githubusercontent.com/fer-rouco/random-quote-machine/main/assets/screenshot.png',
+    altImg:'Random quote machine Image',
+    hrefCode:'https://github.com/fer-rouco/random-quote-machine/',
+    hrefLive:'https://fer-rouco.github.io/random-quote-machine/',
+    tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript]
   }
 ];
 
 function ProjectsSection() {
+  
+  const responsive = {
+    XXLDesktop: {
+      breakpoint: { max: 2688, min: 2304 },
+      items: 6
+    },
+    XXLDesktop: {
+      breakpoint: { max: 2304, min: 1920 },
+      items: 5
+    },
+    XLDesktop: {
+      breakpoint: { max: 1920, min: 1536 },
+      items: 4
+    },
+    desktop: {
+      breakpoint: { max: 1536, min: 1152 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1152, min: 768 },  
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 768, min: 0 },
+      items: 1
+    }
+  };
   return (
     <section id='projects-section' className='projects-section' >
       
       <h2 className='projects-section__header' >Some things I've worked on</h2>
 
       <div className='projects-section__cards' >
-        {PROJECTS.map(project => (
-          <Card
-            key={project.key}
-            title={project.title}
-            description={project.description}
-            srcImg={project.srcImg}
-            altImg={project.altImg}
-            hrefCode={project.hrefCode}
-            hrefLive={project.hrefLive}
-            tags={project.tags}>
-          </Card>
-        ))}
+        <Carousel
+          showDots={true}
+          draggable={false}
+          infinite={false}
+          keyBoardControl={true}
+          transitionDuration={50}
+          itemClass="projects-section__cards-item"
+          responsive={responsive}
+        >
+          {PROJECTS.map(project => (
+            <Card
+              key={project.key}
+              title={project.title}
+              description={project.description}
+              srcImg={project.srcImg}
+              altImg={project.altImg}
+              hrefCode={project.hrefCode}
+              hrefLive={project.hrefLive}
+              tags={project.tags}>
+            </Card>
+          ))}
+        </Carousel>
       </div>
 
     </section>
