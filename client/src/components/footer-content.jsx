077d011d41b73a9c6e0d84b739b87004de2bb026
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2024-02-07 16:26:59 -0300

    Codewars badge added

--- client/src/components/footer-content.jsx
+++ client/src/components/footer-content.jsx
@@ -3,11 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
-function ProfileLink({ id, href, icon, ariaLabel }) {
+function ProfileLink({ id, href, children, ariaLabel }) {
   return (
     <a id={id} target='_blank' href={href} name="profile-link" aria-label={`${ariaLabel} || ${id} Link`} >
-      <FontAwesomeIcon icon={icon} beatFade={true} tabIndex="0" ></FontAwesomeIcon>
+      {children}
     </a>
+  );
+}
+
+function ProfileLinkIcon({ id, href, icon, ariaLabel }) {
+  return (
+    <ProfileLink>
+      <FontAwesomeIcon icon={icon} beatFade={true} tabIndex="0" ></FontAwesomeIcon>
+    </ProfileLink>
   );
 }
 
@@ -15,9 +23,12 @@ function FooterContet() {
   return (
     <>
       <div className="social-links" >
-        <ProfileLink id='LinkedIn' icon={faLinkedin} href="https://www.linkedin.com/in/fer-rouco"></ProfileLink>
-        <ProfileLink id='GitHub'   icon={faGithub}   href="https://github.com/fer-rouco"></ProfileLink>
-        <ProfileLink id='Mail'     icon={faEnvelope} href="mailto:rouco.fernando@gmail.com"></ProfileLink>
+        <ProfileLinkIcon id='LinkedIn' icon={faLinkedin} href="https://www.linkedin.com/in/fer-rouco"></ProfileLinkIcon>
+        <ProfileLinkIcon id='GitHub'   icon={faGithub}   href="https://github.com/fer-rouco"></ProfileLinkIcon>
+        <ProfileLinkIcon id='Mail'     icon={faEnvelope} href="mailto:rouco.fernando@gmail.com"></ProfileLinkIcon>
+        <ProfileLink id='LinkedIn'                       href="https://www.codewars.com/users/fer.rouco">
+          <img alt="Light Badge (micro)" class="codewars-badge" src="https://www.codewars.com/users/fer.rouco/badges/micro"></img>
+        </ProfileLink>
       </div>
       <div className='copyrights' >
         <span>&#169; Fernando Nicolás Rouco</span>
