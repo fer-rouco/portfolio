c6d5b54bbccabe39e41bbd4a7e1893c86b6b04f5
Fernando Nicolás Rouco <rouco.fernando@gmail.com>
2024-02-09 22:03:32 -0300

    Welcome section with shoting stars

--- client/src/components/sections/welcome-section.jsx
+++ client/src/components/sections/welcome-section.jsx
@@ -1,19 +1,34 @@
-import {useRef} from 'react';
 import "./welcome-section.scss";
 import { ReactComponent as Waves } from '../../assets/waves.svg';
 
 function WelcomeSection() {
   const isDarkMode = true;
 
-  const renderStars = () => {
+  const renderStaticStars = () => {
     return (
       [...Array(1000)].map((x, i) => {
         const posX = Math.random() * window.innerWidth;
         const posY = Math.random() * 800;
         const alfa = Math.random();
         const dimension = (Math.floor(Math.random() * 5) + 1) + 'px';
-        return <div className="welcome-section__star" style={{ left: posX + 'px', top: posY + 'px', opacity: alfa, width: dimension, height: dimension }} ></div>
+        const style = {
+          left: posX + 'px',
+          top: posY + 'px',
+          opacity: alfa,
+          width: dimension,
+          height: dimension
+        };
+        return <div className="welcome-section__star" style={style} ></div>
       })
+    );
+  }
+
+  const renderShotingStars = () => {
+    return (
+      <>
+        <div className="welcome-section__star shoting shoting-to-right" ></div>
+        <div className="welcome-section__star shoting shoting-to-left" ></div>
+      </>
     );
   }
 
@@ -29,10 +44,10 @@ function WelcomeSection() {
   const renderMainStarMoon = (darkMode) => {
     return (
       <div className={`welcome-section__main-star welcome-section__moon`}>
-        <div class="crater"></div>
-        <div class="crater"></div>
-        <div class="crater"></div>
-        <div class="crater"></div>
+        <div className="crater"></div>
+        <div className="crater"></div>
+        <div className="crater"></div>
+        <div className="crater"></div>
       </div>
     );
   }
@@ -53,7 +68,8 @@ function WelcomeSection() {
   const renderDarkMode = () => {
     return (
       <>
-        { renderStars() }
+        { renderStaticStars() }
+        { renderShotingStars() }
         { renderMainStar(isDarkMode) }
       </>
     );
@@ -71,7 +87,7 @@ function WelcomeSection() {
     <section id='welcome-section' className={`welcome-section welcome-section-${isDarkMode ? "dark" : "light"}`} >
       { renderTextContainer() }
       { (isDarkMode) ? renderDarkMode() : renderLightMode() }
-      <Waves></Waves>
+      { <Waves></Waves> }
     </section>
   );
 }
