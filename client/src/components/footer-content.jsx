7833584d8d1c22d5d5b4fb70dd921e7ea2517737
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-09-20 08:50:56 -0300

    Codepen in footer

--- client/src/components/footer-content.jsx
+++ client/src/components/footer-content.jsx
@@ -25,10 +25,13 @@ function FooterContet() {
   return (
     <div className={`footer ${theme}`}>
       <div className="social-links" >
+        <ProfileLinkIcon id='Mail'     icon={faEnvelope} href="mailto:rouco.fernando@gmail.com"></ProfileLinkIcon>
+        <ProfileLinkIcon id='WhatsApp' icon={faWhatsapp} href="https://wa.me/5491161132585"></ProfileLinkIcon>
         <ProfileLinkIcon id='LinkedIn' icon={faLinkedin} href="https://www.linkedin.com/in/fer-rouco"></ProfileLinkIcon>
         <ProfileLinkIcon id='GitHub'   icon={faGithub}   href="https://github.com/fer-rouco"></ProfileLinkIcon>
-        <ProfileLinkIcon id='WhatsApp' icon={faWhatsapp} href="https://wa.me/5491161132585"></ProfileLinkIcon>
-        <ProfileLinkIcon id='Mail'     icon={faEnvelope} href="mailto:rouco.fernando@gmail.com"></ProfileLinkIcon>
+        <ProfileLink     id='Codepen'                    href="https://codepen.io/fer-rouco/pens/public">
+          <img alt="Codepen" className="codepen-image" src="https://assets.codepen.io/t-1/user-default-avatar.jpg?fit=crop&format=auto&height=512&version=0&width=512" ></img>
+        </ProfileLink>
         <ProfileLink     id='CodeWars'                   href="https://www.codewars.com/users/fer.rouco">
           <img alt="Light Badge (micro)" className="codewars-badge" src="https://www.codewars.com/users/fer.rouco/badges/micro"></img>
         </ProfileLink>
