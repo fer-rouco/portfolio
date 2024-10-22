ec44eca0360cea014d5f7f7d459eb5fc57bbf7e8
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-10-22 13:56:34 -0300

    Changes in projects-section component

--- client/src/components/sections/projects-section.jsx
+++ client/src/components/sections/projects-section.jsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import { useTheme } from '../../contexts/theme-context';
-import Card from "../general/card";
+import ProjectCard from "../general/project-card";
 import { TECH } from "../general/tag";
 import "./projects-section.scss";
 
@@ -10,79 +10,80 @@ const PROJECTS = [
   {
     key: 'portfolio',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/portfolio/main/assets/Portfolio.png',
-    hrefCode: 'https://github.com/fer-rouco/portfolio/',
-    hrefLive: 'https://fer-rouco.github.io/portfolio/',
+    codeHref: 'https://github.com/fer-rouco/portfolio/',
+    liveHref: 'https://fer-rouco.github.io/portfolio/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Sass],
     hidden: true
   },
   {
     key: 'minesweeper',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/minesweeper/main/src/assets/images/Board.png',
-    hrefCode: 'https://github.com/fer-rouco/minesweeper/',
-    hrefLive: 'https://fer-rouco.github.io/minesweeper/',
+    codeHref: 'https://github.com/fer-rouco/minesweeper/',
+    liveHref: 'https://fer-rouco.github.io/minesweeper/',
     tags: [TECH.HTML, TECH.CSS3, TECH.TypeScript, TECH.Angular, TECH.Sass]
   },
   {
     key: 'marvel-characters',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/marvel-characters/main/assets/marvel-characters.png',
-    hrefCode: 'https://github.com/fer-rouco/marvel-characters/',
-    hrefLive: 'https://fer-rouco.github.io/marvel-characters/',
+    codeHref: 'https://github.com/fer-rouco/marvel-characters/',
+    liveHref: 'https://fer-rouco.github.io/marvel-characters/',
+    liveTooltip: <div><span>User: msuarez@gmail.com</span><span>Pass: MSuarez</span></div>,
     tags: [TECH.HTML, TECH.CSS3, TECH.TypeScript, TECH.Angular, TECH.Sass],
   },
   {
     key: 'bank-products',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/bank-products/main/assets/product-list.png',
-    hrefCode: 'https://github.com/fer-rouco/bank-products/',
-    // hrefLive: 'https://fer-rouco.github.io/bank-products/',
+    codeHref: 'https://github.com/fer-rouco/bank-products/',
+    // liveHref: 'https://fer-rouco.github.io/bank-products/',
     tags: [TECH.HTML, TECH.CSS3, TECH.TypeScript, TECH.Angular, TECH.Sass]
   },
   {
     key: 'ncr-challenge',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/ncr-challenge/main/assets/ncr-challenge.png',
-    hrefCode: 'https://github.com/fer-rouco/ncr-challenge/',
-    // hrefLive: 'https://fer-rouco.github.io/ncr-challenge/',
+    codeHref: 'https://github.com/fer-rouco/ncr-challenge/',
+    // liveHref: 'https://fer-rouco.github.io/ncr-challenge/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.StyledComponents, TECH.Axios, TECH.AmCharts, TECH.PopperJs],
   },
   {
     key: 'eldar-challenge',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/eldar-challenge/main/assets/product-list.png',
-    hrefCode: 'https://github.com/fer-rouco/eldar-challenge/',
-    // hrefLive: 'https://fer-rouco.github.io/eldar-challenge/',
+    codeHref: 'https://github.com/fer-rouco/eldar-challenge/',
+    // liveHref: 'https://fer-rouco.github.io/eldar-challenge/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.StyledComponents, TECH.Axios, TECH.Node, TECH.Express, TECH.Mongoose]
   },
   {
     key: 'markdown-previewer',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/markdown-previewer/main/src/assets/screenshot.png',
-    hrefCode: 'https://github.com/fer-rouco/markdown-previewer/',
-    hrefLive: 'https://fer-rouco.github.io/markdown-previewer/',
+    codeHref: 'https://github.com/fer-rouco/markdown-previewer/',
+    liveHref: 'https://fer-rouco.github.io/markdown-previewer/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.Sass]
   },
   {
     key: 'drum-machine',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/drum-machine/main/src/assets/screenshot.png',
-    hrefCode: 'https://github.com/fer-rouco/drum-machine/',
-    hrefLive: 'https://fer-rouco.github.io/drum-machine/',
+    codeHref: 'https://github.com/fer-rouco/drum-machine/',
+    liveHref: 'https://fer-rouco.github.io/drum-machine/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.StyledComponents]
   },
   {
     key: 'javascript-calculator',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/calculator/main/src/assets/screenshot.png',
-    hrefCode: 'https://github.com/fer-rouco/calculator/',
-    hrefLive: 'https://fer-rouco.github.io/calculator/',
+    codeHref: 'https://github.com/fer-rouco/calculator/',
+    liveHref: 'https://fer-rouco.github.io/calculator/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Sass]
   },
   {
     key: 'pomodoro-clock',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/25-5-clock/main/src/assets/screenshot.png',
-    hrefCode: 'https://github.com/fer-rouco/25-5-clock/',
-    hrefLive: 'https://fer-rouco.github.io/25-5-clock/',
+    codeHref: 'https://github.com/fer-rouco/25-5-clock/',
+    liveHref: 'https://fer-rouco.github.io/25-5-clock/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript, TECH.React, TECH.Redux, TECH.Sass]
   },
   {
     key: 'random-quote-machine',
     srcImg: 'https://raw.githubusercontent.com/fer-rouco/random-quote-machine/main/assets/screenshot.png',
-    hrefCode: 'https://github.com/fer-rouco/random-quote-machine/',
-    hrefLive: 'https://fer-rouco.github.io/random-quote-machine/',
+    codeHref: 'https://github.com/fer-rouco/random-quote-machine/',
+    liveHref: 'https://fer-rouco.github.io/random-quote-machine/',
     tags: [TECH.HTML, TECH.CSS3, TECH.JavaScript],
     hidden: true
   }
@@ -139,16 +140,17 @@ function ProjectsSection() {
           {PROJECTS_WITH_TRANSLATIONS
           .filter(project => !project.hidden)
           .map(project => (
-            <Card
+            <ProjectCard
               key={project.key}
               title={project.title}
               description={project.description}
               srcImg={project.srcImg}
               altImg={project.altImg}
-              hrefCode={project.hrefCode}
-              hrefLive={project.hrefLive}
+              codeHref={project.codeHref}
+              liveHref={project.liveHref}
+              liveTooltip={project.liveTooltip}
               tags={project.tags}>
-            </Card>
+            </ProjectCard>
           ))}
         </Carousel>
       </div>
