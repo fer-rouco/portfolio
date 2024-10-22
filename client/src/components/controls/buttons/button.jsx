7072f3e7de98161440472a4413ce15525ec70abb
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-10-22 13:54:21 -0300

    Reverting commit to add tooltip into the button

--- client/src/components/controls/buttons/button.jsx
+++ client/src/components/controls/buttons/button.jsx
@@ -2,12 +2,10 @@ import "./button.scss";
 
 export default function Button({ type, disabled, labelPrefix, children, labelSufix, onClick, onKeyDown, tooltip }) {
   return (
-    <div className="button-container">
-      <button className='button' type={type || 'button'} disabled={disabled} onClick={onClick} onKeyDown={onKeyDown} tabIndex="0" data-tooltip={tooltip} >
-        {labelPrefix}
-        {children}
-        {labelSufix}
-      </button>
-    </div>
+    <button className='button' type={type || 'button'} disabled={disabled} onClick={onClick} onKeyDown={onKeyDown} tabIndex="0" >
+      {labelPrefix}
+      {children}
+      {labelSufix}
+    </button>
   );
 }
